6b1054836dfea45708edeb4270281ff1d6a6c795
Amlan Pati <amlanpati@gmail.com>
2024-12-31 23:42:23 +0530

    Updated file structure

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,18 +2,17 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect((err) => {
   if (err) {
     console.error("Database connection error:", err.stack);
   } else {
-    console.log("Connected to the database");
+    console.log("Connected to the Neon database");
   }
 });
 
