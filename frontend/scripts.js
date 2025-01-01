045fe2412ac46da2b9d484569d87fb797209be8b
Amlan Pati <amlanpati@gmail.com>
2025-01-01 13:41:10 +0530

    Updated css

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -93,7 +93,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       try {
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+        const response = await fetch("/api/auth/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email, password }),
@@ -156,7 +156,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       try {
         const response = await fetch(
-          "http://localhost:5000/api/auth/register",
+          "/api/auth/register",
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
